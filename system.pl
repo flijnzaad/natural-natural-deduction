ea4534a4a2b85655b91e796a2808472d02c40827
Flip Lijnzaad <f.a.e.lijnzaad@student.rug.nl>
2021-03-10 17:05:14 +0100

    0.3: Failed attempt at solving infinite recursion problem. Addition of derive/3

--- system.pl
+++ system.pl
@@ -2,7 +2,7 @@
 %                 NATURAL NATURAL DEDUCTION THEOREM PROVER                   %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % by Flip Lijnzaad %
-%   version 0.2    %
+%   version 0.3    %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % TODO: possibly add more information to the predicate?
 %       that will be necessary anyways upon introduction of subproofs.
@@ -32,6 +32,7 @@
 
 % The premises need to be asserted into the database
 % provable(1, and(p, q), premise, []).
+:- dynamic provable/3.
 provable(1, and(p, q), premise).
 % The conclusion needs to be queried by the user like so:
 % provable(LineNr, Conclusion, Justification).
@@ -40,42 +41,45 @@ provable(1, and(p, q), premise).
 % that contains a conjunction with X as its first conjunct"
 % provable(Current, X, conjElim, [H|T]) :-
 provable(Current, X, conjElim) :-
-    % but also you want to have some way to determine the exact line numbers
-    % Current > Previous,
+    Current > 0,
     Previous is Current - 1,
     Previous > 0,
     % H = provable(Previous, and(X, _), _),
-    provable(Previous, and(X, _), _), !.
+    provable(Previous, and(X, Y), _), !, nonvar(Y), !.
 
 % commutativity of and: and(X, Y) <=> and(Y, X)
 provable(Current, X, conjElim) :-
     % Current > Previous,
-    Previous is Current -1,
+    Current > 0,
+    Previous is Current - 1,
     Previous > 0,
-    provable(Previous, and(_, X), _).
+    provable(Previous, and(Y, X), _), nonvar(Y), !.
 
 % "A disjunction with X as its left disjunct is provable by 
 % disjunction introduction if there is a previous line that contains X"
 provable(Current, or(X, _), disjIntro) :-
     % Current > Previous,
+    Current > 0,
     Previous is Current -1,
     Previous > 0,
-    provable(Previous, X, _).
+    provable(Previous, X, _), !.
 
 % commutativity of or: or(X, Y) <=> or(Y, X)
 provable(Current, or(_, X), disjIntro) :-
     % Current > Previous,
+    Current > 0,
     Previous is Current - 1,
     Previous > 0,
-    provable(Previous, X, _).
+    provable(Previous, X, _), !.
 
 % "X is provable by negation elimination if there is a previous line
 % that contains the twice negated version of X"
 provable(Current, X, negElim) :-
     % Current > Previous,
+    Current > 0,
     Previous is Current - 1,
     Previous > 0,
-    provable(Previous, not(not(X)), _).
+    provable(Previous, not(not(X)), _), !.
 
 % "A conjunction of X and Y is provable by conjunction introduction 
 % if there is a previous line that contains the first conjunct and
@@ -87,3 +91,25 @@ provable(Current, and(X, Y), conjIntro) :-
     % Current > Previous2,
     provable(Previous1, X, _),
     provable(Previous2, Y, _).
+
+% turning the predicate around
+:- dynamic derive/3.
+% conclusion
+derive(_, and(p, q), _).
+% query the premises
+
+derive(Previous, and(X, _), _) :-
+    Current is Previous + 1,
+    derive(Current, X, conjElim), !.
+
+% same problem as with provable: this rule can go on forever
+derive(Previous, X, _) :-
+    Current is Previous + 1,
+    derive(Current, or(X, _), disjIntro), !.
+
+derive(_, X, _) :-
+    derive(Previous2, Y, _),
+    Current is Previous2 + 1,
+    derive(Current, and(X, Y), conjIntro), !.
+
+% problem here: a Prolog rule can only have one predicate as its rule head
