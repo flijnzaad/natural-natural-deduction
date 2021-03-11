ce115c3ac0b0fa1cdf25541c7a550f830123f87a
Flip Lijnzaad <f.a.e.lijnzaad@student.rug.nl>
2021-03-11 15:29:06 +0100

    Tried something in Python

---
+++ system.py
@@ -0,0 +1,16 @@
+premises = ["p", "q"]
+conclusion = "and(p, q)"
+
+def conj_intro(a, b):
+    result = "and(" + a + ", " + b + ")"
+    return result
+
+def conj_elim(a):
+    result =
+
+proof_lines = premises
+
+while conclusion not in proof_lines:
+    do something
+
+
