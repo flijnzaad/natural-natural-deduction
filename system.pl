7b85ef4419e2db7afd445536c511ebfc400b6ce2
Flip Lijnzaad <f.a.e.lijnzaad@student.rug.nl>
2021-03-29 09:54:01 +0200

    0.17: Attempt at fixing infinite recursion, added FIXMEs

--- system.pl
+++ system.pl
@@ -2,101 +2,90 @@
 %                 NATURAL NATURAL DEDUCTION THEOREM PROVER                   %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % by Flip Lijnzaad %
-%   version 0.16   %
+%   version 0.17   %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % TODO: add the list of all previous lines to the predicate
-% TODO: forward and backward search
+%       --> FIXME: make it better, this implementation doesn't really work
 % TODO: add a cut to the rule bodies but justify it theoretically
 % TODO: how to get rid of lines that turned out to be unnecessary in the proof?
 %       will that be a problem at all?
 % TODO: add line number support
 
-% Current problems:
-%     * q3-7 always go into infinite recursion, no matter the bound: why?
+% FIXME: q3-7 always go into infinite recursion, no matter the bound: why?
+%        --> only when the reverse/2 at the end is included; else only q4.
+%        when reverse/2 is included, the conclusion and all needed proof lines
+%        are in proofs.txt, but the program never exits recursion.
+% FIXME: q4 always goes into infinite recursion: or(p, p) is never introduced,
+%        only "higher-level" disjunctions are present
 
 % proves(X, Y) where X is a list of previous proof lines and Y is a formula
 % that can be proven from X
 % line(X, Y) where X is the formula on the line, Y is the justification
 
 % conjunction elimination:
-proves(Premises, Line, _) :-
-    Line = line(X, conjElim),
+proves(Premises, line(X, conjElim), _) :-
     member(line(and(X,_), _), Premises).
 
-proves(Premises, Line, _) :-
-    Line = line(Y, conjElim),
+proves(Premises, line(Y, conjElim), _) :-
     member(line(and(_,Y), _), Premises).
 
 % conjunction introduction:
-proves(Premises, Line, _) :-
-    Line = line(and(X,Y), conjIntro),
+proves(Premises, line(and(X,Y), conjIntro), _) :-
     member(line(X, _), Premises),
     member(line(Y, _), Premises).
 
 % disjunction introduction:
-proves(Premises, Line, _) :-
-    Line = line(or(X, Y), disjIntro),
-    nonvar(Y),
+proves(Premises, line(or(X, _), disjIntro), _) :-
     member(line(X, _), Premises).
 
-proves(Premises, Line, _) :-
-    Line = line(or(X, Y), disjIntro),
-    nonvar(X),
+proves(Premises, line(or(_, Y), disjIntro), _) :-
     member(line(Y, _), Premises).
 
 % implication elimination:
-proves(Premises, Line, _) :-
-    Line = line(Y, impElim),
+proves(Premises, line(Y, impElim), _) :-
     member(line(if(X, Y), _), Premises),
     member(line(X, _), Premises).
 
 % bi-implication elimination:
-proves(Premises, Line, _) :-
-    Line = line(Y, biimpElim),
+proves(Premises, line(Y, biimpElim), _) :-
     member(line(iff(X, Y), _), Premises),
     member(line(X, _), Premises).
 
-proves(Premises, Line, _) :-
-    Line = line(X, biimpElim),
+proves(Premises, line(X, biimpElim), _) :-
     member(line(iff(X, Y), _), Premises),
     member(line(Y, _), Premises).
 
 % negation elimination:
-proves(Premises, Line, _) :-
-    Line = line(X, negElim),
+proves(Premises, line(X, negElim), _) :-
     member(line(neg(neg(X)), _), Premises).
 
 % contradiction introduction:
-proves(Premises, Line, _) :-
-    Line = line(contra, contraIntro),
+proves(Premises, line(contra, contraIntro), _) :-
     member(line(X, _), Premises),
     member(line(neg(X), _), Premises).
 
 % transitivity: => recursion
-proves(Premises, LineX, All) :-
+proves(Premises, line(X, JustX), All) :-
     length(Premises, N),
-    N < 5,                               % bound the number of proof steps
-    LineX = line(_, _),
-    LineY = line(Y, _),
-    proves(Premises, LineY, All),
+    N < 4,                               % bound the number of proof steps
+    proves(Premises, line(Y, JustY), All),
     \+ member(line(Y, _), Premises),     % don't prove lines you already have
-    New = [LineY|Premises],
-    proves(New, LineX, All),
-    printline(N, LineY),
-    printline(N, LineX),
-    Newer = [LineX|New],
-    reverse(Newer, All).
-    % write(All), nl.
+    New = [line(Y, JustY)|Premises],
+    proves(New, line(X, JustX), All),
+    printline(N, line(Y, JustY)),
+    printline(N, line(X, JustX)).
+    % All = [line(X, JustX)|New].
+    % Newer = [line(X, JustX)|New],
+    % reverse(Newer, All).
+    % writeln(All).
 
 % reiteration / stop when goal reached:
-proves(Premises, Line, _) :-
-    Line = line(X, reit),
+proves(Premises, line(X, reit), _) :-
     member(line(X, _), Premises).
 
 % contradiction elimination:
-proves(Premises, Line, _) :-
-    Line = line(_, contraElim),
+proves(Premises, line(_, contraElim), _) :-
     member(line(contra, _), Premises).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -158,7 +147,7 @@ q4 :-
 
 q4(X) :-
     Premises = [line(and(p, q), premise)],
-    Concl = line(and(and(q, and(p, p)), or(r, p)), _),
+    Concl = line(and(and(q, and(p, p)), or(p, p)), _),
     proves(Premises, Concl, X).
 
 q5 :-
