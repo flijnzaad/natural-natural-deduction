b51549fa72705f7a4bb45469466f82d108cf8896
Flip Lijnzaad <f.a.e.lijnzaad@student.rug.nl>
2021-04-23 12:44:49 +0200

    1.1: Naive iterative deepening, doesn't work

--- system.pl
+++ system.pl
@@ -2,82 +2,98 @@
 %                 NATURAL NATURAL DEDUCTION THEOREM PROVER                   %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % by Flip Lijnzaad %
-%   version 1.0    %
+%   version 1.1    %
 %%%%%%%%%%%%%%%%%%%%
 
-% TODO: implement iterative deepening
-% TODO: add more cuts to the rule bodies but justify it theoretically:
-%       right now it lets you take another option
 % TODO: add line number support
 
 % FIXME: add more testing queries that are more elaborate (maybe substitute some,
 %        because some of the current ones are nearly identical, keep the hardest
 %        ones)
 
+% This makes sure that answers are never abbreviated with "..."
+:- set_prolog_flag(answer_write_options,
+                    [ quoted(true),
+                      portray(true),
+                      spacing(next_argument)
+                    ]).
+
 % conjunction elimination:
-proves(Premises, line(X, conjElim), [line(X, conjElim)|Premises]) :-
+proves(Premises, line(X, conjElim), [line(X, conjElim)|Premises], _) :-
     member(line(and(X,_), _), Premises).
 
-proves(Premises, line(Y, conjElim), [line(Y, conjElim)|Premises]) :-
+proves(Premises, line(Y, conjElim), [line(Y, conjElim)|Premises], _) :-
     member(line(and(_,Y), _), Premises).
 
 % conjunction introduction:
-proves(Premises, line(and(X, Y), conjIntro), [line(and(X,Y), conjIntro)|Premises]) :-
+proves(Premises, line(and(X, Y), conjIntro), [line(and(X,Y), conjIntro)|Premises], _) :-
     member(line(X, _), Premises),
     member(line(Y, _), Premises).
 
 % disjunction introduction:
-proves(Premises, line(or(X, Y), disjIntro), [line(or(X, Y), disjIntro)|Premises]) :-
+proves(Premises, line(or(X, Y), disjIntro), [line(or(X, Y), disjIntro)|Premises], _) :-
     member(line(X, _), Premises).
 
-proves(Premises, line(or(X, Y), disjIntro), [line(or(X, Y), disjIntro)|Premises]) :-
+proves(Premises, line(or(X, Y), disjIntro), [line(or(X, Y), disjIntro)|Premises], _) :-
     member(line(Y, _), Premises).
 
 % implication elimination:
-proves(Premises, line(Y, impElim), [line(Y, impElim)|Premises]) :-
+proves(Premises, line(Y, impElim), [line(Y, impElim)|Premises], _) :-
     member(line(if(X, Y), _), Premises),
     member(line(X, _), Premises).
 
 % bi-implication elimination:
-proves(Premises, line(Y, biimpElim), [line(Y, biimpElim)|Premises]) :-
+proves(Premises, line(Y, biimpElim), [line(Y, biimpElim)|Premises], _) :-
     member(line(iff(X, Y), _), Premises),
     member(line(X, _), Premises).
 
-proves(Premises, line(X, biimpElim), [line(X, biimpElim)|Premises]) :-
+proves(Premises, line(X, biimpElim), [line(X, biimpElim)|Premises], _) :-
     member(line(iff(X, Y), _), Premises),
     member(line(Y, _), Premises).
 
 % negation elimination:
-proves(Premises, line(X, negElim), [line(X, negElim)|Premises]) :-
+proves(Premises, line(X, negElim), [line(X, negElim)|Premises], _) :-
     member(line(neg(neg(X)), _), Premises).
 
 % contradiction introduction:
-proves(Premises, line(contra, contraIntro), [line(contra, contraIntro)|Premises]) :-
+proves(Premises, line(contra, contraIntro), [line(contra, contraIntro)|Premises], _) :-
     member(line(X, _), Premises),
     member(line(neg(X), _), Premises).
 
-% transitivity: => recursion
-proves(Premises, line(X, JustX), End) :-
-    % bound the number of proof lines
-    length(Premises, N),
-    N < 6,
-    % derive 1 step from the premises, matching the simple rules
-    proves(Premises, line(_, _), New),
-    % this either matches with the base cases or goes into transitivity again
-    proves(New, line(X, JustX), End), !.
+% contradiction elimination:
+proves(Premises, line(X, contraElim), [line(X, contraElim)|Premises], _) :-
+    member(line(contra, _), Premises).
 
-% reiteration / stop when goal reached:
-proves(Premises, line(X, reit), [line(X, reit)|Premises]) :-
+% reiteration:
+proves(Premises, line(X, reit), [line(X, reit)|Premises], _) :-
     member(line(X, _), Premises).
 
-% contradiction elimination:
-proves(Premises, line(X, contraElim), [line(X, contraElim)|Premises]) :-
-    member(line(contra, _), Premises).
+% base case for iterative deepening: if maximum search depth reached,
+% cut the other branches and evaluate to false
+proves(_, _, _, D) :-
+    D >= 10, !, fail.
+
+% transitivity:
+proves(Premises, line(X, JustX), End, D) :-
+    % bound the number of proof lines
+    length(Premises, N),
+    N < D,
+    % derive 1 line from the premises
+    proves(Premises, line(_, _), New, D),
+    % with this step added to the premises, derive line X
+    proves(New, line(X, JustX), End, D), !.
+
+% recursive case for iterative deepening
+proves(Premises, Line, New, D) :-
+    Dnew is D + 1,
+    write('D = '), writeln(Dnew),
+    write('Premises: '), writeln(Premises),
+    proves(Premises, Line, New, Dnew).
 
 % provesWrap/3 takes care of reversing the premises,
 % then calls proves/3 to do the proving,
 % then reverses the final proof in the end
 provesWrap(Premises, Conclusion, X) :-
     reverse(Premises, P),
-    proves(P, Conclusion, Y),
+    proves(P, Conclusion, Y, 1),
     reverse(Y, X).
