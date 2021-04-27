65db711c103b8c46b8192ad21b8fce0842f45c82
Flip Lijnzaad <f.a.e.lijnzaad@student.rug.nl>
2021-04-27 17:55:20 +0200

    Removed old things, tried something

--- iterative_deepening.pl
+++ iterative_deepening.pl
@@ -6,7 +6,7 @@ deep(List, X, N) :-
     length(List, M),
     M < N,
     Y = [a|List],
-    deep(Y, X, N), !.
+    deep(Y, Y, N), !.
 
 deep(List, X, N) :-
     New is N + 1,
@@ -14,22 +14,3 @@ deep(List, X, N) :-
 
 deep(X) :-
     deep([], X, 1).
-
-id(S, Path) :-
-    from(Limit, 1, 5),
-    id1(S, 0, Limit, Path).
-
-id1(S, Depth, Limit, [S]) :-
-    Depth < Limit,
-    goal(S).
-
-id1(S, Depth, Limit, [S|Rest]) :-
-    Depth < Limit,
-    Depth2 is Depth + 1,
-    arc(S, S2),
-    id1(S2, Depth2, Limit, Rest).
-
-from(X, X, Inc).
-from(X, N, Inc) :-
-    N2 is N + Inc,
-    from(X, N2, Inc).
