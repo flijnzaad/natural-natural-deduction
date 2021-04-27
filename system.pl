bdebdb7d506976aee9020c00540a22ac2333967e
Flip Lijnzaad <f.a.e.lijnzaad@student.rug.nl>
2021-04-27 12:20:39 +0200

    1.2: Attempt at fixing problems with onlyPremises

--- system.pl
+++ system.pl
@@ -2,10 +2,12 @@
 %                 NATURAL NATURAL DEDUCTION THEOREM PROVER                   %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % by Flip Lijnzaad %
-%   version 1.1    %
+%   version 1.2    %
 %%%%%%%%%%%%%%%%%%%%
 
 % TODO: add line number support
+% TODO: see https://staff.fnwi.uva.nl/u.endriss/teaching/prolog/prolog.pdf
+% page 43 for maybe defining infix operators for things
 
 % FIXME: add more testing queries that are more elaborate (maybe substitute some,
 %        because some of the current ones are nearly identical, keep the hardest
@@ -68,16 +70,24 @@ proves(Premises, line(X, contraElim), [line(X, contraElim)|Premises], _) :-
 proves(Premises, line(X, reit), [line(X, reit)|Premises], _) :-
     member(line(X, _), Premises).
 
+proves(Premises, _, _, D) :-
+    % bound the number of proof lines
+    length(Premises, N),
+    N > D, !,
+    write('D = '), write(D), writeln('; depth exceeded, backtrack'),
+    tab(7), write('Premises = '), writeln(Premises),
+    fail.
+
 % base case for iterative deepening: if maximum search depth reached,
-% cut the other branches and evaluate to false
+% cut the other branches and evaluate to false to force backtracking
 proves(_, _, _, D) :-
-    D >= 10, !, fail.
+    D >= 5, !, fail.
 
 % transitivity:
 proves(Premises, line(X, JustX), End, D) :-
     % bound the number of proof lines
     length(Premises, N),
-    N < D,
+    N =< D,
     % derive 1 line from the premises
     proves(Premises, line(_, _), New, D),
     % with this step added to the premises, derive line X
@@ -87,13 +97,31 @@ proves(Premises, line(X, JustX), End, D) :-
 proves(Premises, Line, New, D) :-
     Dnew is D + 1,
     write('D = '), writeln(Dnew),
-    write('Premises: '), writeln(Premises),
-    proves(Premises, Line, New, Dnew).
+    write('Premises deep:  '), writeln(Premises),
+    onlyPremises(Premises, P),
+    proves(P, Line, New, Dnew).
 
-% provesWrap/3 takes care of reversing the premises,
+% provesWrap/3 reverses the premises,
 % then calls proves/3 to do the proving,
-% then reverses the final proof in the end
+% then reverses the final resulting proof in the end
 provesWrap(Premises, Conclusion, X) :-
     reverse(Premises, P),
-    proves(P, Conclusion, Y, 1),
+    length(Premises, N),
+    proves(P, Conclusion, Y, N),
     reverse(Y, X).
+
+% if you don't have any lines left, you're done
+onlyPremises([], R, R).
+
+% a line that's not a premise shouldn't be included; then you're done
+onlyPremises([line(_, J)|_], R, R) :-
+    J \= premise.
+
+% add each premise to the list A
+onlyPremises([line(X, premise)|T], A, R) :-
+    onlyPremises(T, [line(X, premise)|A], R).
+
+onlyPremises(Premises, Result) :-
+    % get the `normal' order, with the premises at the start
+    reverse(Premises, P),
+    onlyPremises(P, [], Result), !.
