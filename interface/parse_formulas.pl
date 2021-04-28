430e3c057a04affff41b17b493cdfd82a8cb88e3
Flip Lijnzaad <f.a.e.lijnzaad@student.rug.nl>
2021-04-28 17:55:50 +0200

    Added concatBinary for generalization, added all connectives and contradiction

--- interface/parse_formulas.pl
+++ interface/parse_formulas.pl
@@ -1,16 +1,35 @@
+concatBinary(StringX, StringY, String) :-
+    atomics_to_string(["(", StringX, "\\land", StringY, ")"], " ", String).
+
 % TODO: research the difference between single and double quotes
-stringOf(p,'P').
-stringOf(q,'Q').
-stringOf(r,'R').
-stringOf(s,'S').
+stringOf(p, "P").
+stringOf(q, "Q").
+stringOf(r, "R").
+stringOf(s, "S").
+stringOf(contra, "\\lfalse").
 
 % no parentheses needed around negated sentences because all binary connectives
 % put parentheses around the subparts already
 stringOf(neg(X), String) :-
     stringOf(X, StringX),
-    atomics_to_string(['\\lnot', StringX], ' ', String).
+    atomics_to_string(["\\lnot", StringX], " ", String).
 
 stringOf(and(X, Y), String) :-
     stringOf(X, StringX),
     stringOf(Y, StringY),
-    atomics_to_string(['(', StringX, '\\land', StringY, ')'], ' ', String).
+    concatBinary(StringX, StringY, String).
+
+stringOf(or(X, Y), String) :-
+    stringOf(X, StringX),
+    stringOf(Y, StringY),
+    concatBinary(StringX, StringY, String).
+
+stringOf(if(X, Y), String) :-
+    stringOf(X, StringX),
+    stringOf(Y, StringY),
+    concatBinary(StringX, StringY, String).
+
+stringOf(iff(X, Y), String) :-
+    stringOf(X, StringX),
+    stringOf(Y, StringY),
+    concatBinary(StringX, StringY, String).
