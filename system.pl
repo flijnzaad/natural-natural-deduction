e76eab068610a68adf1f359fb09fb03e1e2d90d4
Flip Lijnzaad <f.a.e.lijnzaad@student.rug.nl>
2021-04-28 11:35:21 +0200

    1.3: Fixed iterative deepening (rough)

--- system.pl
+++ system.pl
@@ -1,4 +1,4 @@
-% version 1.2
+% version 1.3
 
 % This makes sure that answers are never abbreviated with "..."
 :- set_prolog_flag(answer_write_options,
@@ -57,18 +57,18 @@ proves(Premises, line(X, contraElim), [line(X, contraElim)|Premises], _) :-
 proves(Premises, line(X, reit), [line(X, reit)|Premises], _) :-
     member(line(X, _), Premises).
 
-proves(Premises, _, _, D) :-
+proves(Premises, Line, New, D) :-
     % bound the number of proof lines
     length(Premises, N),
     N > D, !,
-    write('D = '), write(D), writeln('; depth exceeded, backtrack'),
-    tab(7), writeln(Premises),
-    fail.  % force backtracking FIXME: why doesn't this work?
+    % write('D = '), write(D), writeln('; depth exceeded, backtrack'),
+    % tab(7), writeln(Premises),
+    fail. % force backtracking FIXME: why doesn't this work?
 
 % base case for iterative deepening: if maximum search depth reached,
 % cut the other branches and evaluate to false to force backtracking
-proves(_, _, _, D) :-
-    D >= 7, !, fail.
+% provesIDS(_, _, _, D) :-
+%     D >= 7, !, fail.
 
 % transitivity:
 proves(Premises, line(X, JustX), End, D) :-
@@ -81,11 +81,16 @@ proves(Premises, line(X, JustX), End, D) :-
     proves(New, line(X, JustX), End, D), !.
 
 % recursive case for iterative deepening
-proves(Premises, Line, New, D) :-
+provesIDS(Premises, Line, New, D) :-
+    proves(Premises, Line, New, D), !.
+
+% recursive case for iterative deepening
+provesIDS(Premises, Line, New, D) :-
     Dnew is D + 1,
-    write('D = '), writeln(Dnew),
-    write('Premises deep:  '), writeln(Premises),
-    proves(Premises, Line, New, Dnew).
+    Dnew < 10,
+    % write('D = '), writeln(Dnew),
+    % write('Premises deep:  '), writeln(Premises),
+    provesIDS(Premises, Line, New, Dnew).
 
 % provesWrap/3 reverses the premises,
 % then calls proves/3 to do the proving,
@@ -93,5 +98,5 @@ proves(Premises, Line, New, D) :-
 provesWrap(Premises, Conclusion, X) :-
     reverse(Premises, P),
     length(Premises, N),
-    proves(P, Conclusion, Y, N),
+    provesIDS(P, Conclusion, Y, N),
     reverse(Y, X).
