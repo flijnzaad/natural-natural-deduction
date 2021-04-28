855eb86de268575c1c7223d412d0dcaa6fac6fe6
Flip Lijnzaad <f.a.e.lijnzaad@student.rug.nl>
2021-04-28 17:45:10 +0200

    Added disambiguating parentheses and optimized with atomics_to_string/3

--- interface/parse_formulas.pl
+++ interface/parse_formulas.pl
@@ -1,14 +1,16 @@
-stringOf(p,"p").
-stringOf(q,"q").
-stringOf(r,"r").
-stringOf(s,"s").
+% TODO: research the difference between single and double quotes
+stringOf(p,'P').
+stringOf(q,'Q').
+stringOf(r,'R').
+stringOf(s,'S').
 
-stringOf(neg(X),S) :-
-    stringOf(X,SX),
-    string_concat(" \\lnot ", SX, S).
+% no parentheses needed around negated sentences because all binary connectives
+% put parentheses around the subparts already
+stringOf(neg(X), String) :-
+    stringOf(X, StringX),
+    atomics_to_string(['\\lnot', StringX], ' ', String).
 
-stringOf(and(X,Y),S) :-
-    stringOf(X,SX),
-    stringOf(Y,SY),
-    string_concat(SX, " \\land ", S2),
-    string_concat(S2, SY, S).
+stringOf(and(X, Y), String) :-
+    stringOf(X, StringX),
+    stringOf(Y, StringY),
+    atomics_to_string(['(', StringX, '\\land', StringY, ')'], ' ', String).
