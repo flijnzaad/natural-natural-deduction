532a60da36ec582b95748df037b535d0b484dcd1
Flip Lijnzaad <f.a.e.lijnzaad@student.rug.nl>
2021-04-29 11:02:11 +0200

    Changed name of predicate for uniformity

--- interface/parse_formulas.pl
+++ interface/parse_formulas.pl
@@ -6,24 +6,24 @@
                     ]).
 
 % the cut is needed to not consider the other option with capitalization
-stringOf(contra, "\lfalse") :- !.
+stringForm(contra, "\lfalse") :- !.
 
 % parse an atomic sentence to a capitalized string
-stringOf(X, String) :-
+stringForm(X, String) :-
     % only do this for atomic sentences
     atom(X), !,
     % make the string uppercase
     string_upper(X, String).
 
 % parse negation
-stringOf(neg(X), String) :-
-    !, stringOf(X, StringX),
+stringForm(neg(X), String) :-
+    !, stringForm(X, StringX),
     % no parentheses needed around negated sentences because binary connectives
     % put parentheses around the subparts already
     atomics_to_string(["\lnot", StringX], " ", String).
 
 % parse binary connectives
-stringOf(Formula, String) :-
+stringForm(Formula, String) :-
     % get the name of the binary connective
     functor(Formula, Name, 2),
     % put \l in front of it (\land, \lor, etc.)
@@ -31,8 +31,8 @@ stringOf(Formula, String) :-
     % get the arguments of the connective
     arg(1, Formula, X),
     arg(2, Formula, Y),
-    stringOf(X, StringX),
-    stringOf(Y, StringY),
+    stringForm(X, StringX),
+    stringForm(Y, StringY),
     % concatenate the two strings with the connective in between,
     % surrounded by disambiguating parentheses
     atomics_to_string(["(", StringX, Connective, StringY, ")"], " ", String).
