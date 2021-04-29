52206fdc06b19ea90c0ce8d9d9ac9f30eaf3f427
Flip Lijnzaad <f.a.e.lijnzaad@student.rug.nl>
2021-04-29 12:39:57 +0200

    Refactoring; putting all testing queries in the document

--- interface/interface.py
+++ interface/interface.py
@@ -1,19 +1,19 @@
-from pyswip import Prolog
+from pyswip import Prolog       # querying our knowledge bases
 pl = Prolog()
+import os, subprocess, platform # compiling and opening a LaTeX pdf
+import shutil                   # copying the 'preamble.tex' file
 
-# load the knowledge base
-pl.consult("../system.pl")
+pl.consult("../system.pl")      # load the knowledge bases
+pl.consult("../tests.pl")
+pl.consult("build_proof.pl")
 
+# compiling and opening a LaTeX pdf
 def compile_open_pdf(name):
-    # compiling and opening a LaTeX pdf
-    import os, subprocess, platform
-
     compile_command = 'pdflatex ' + name + '.tex'
     # TODO: maybe this is also OS-specific
     os.system(compile_command)
 
     pdf_name = name + '.pdf'
-
     if platform.system() == 'Darwin':           # macOS
         subprocess.call(('open', pdf_name))
     elif platform.system() == 'Windows':        # Windows
@@ -21,17 +21,19 @@ def compile_open_pdf(name):
     else:                                       # linux variants
         subprocess.call(('xdg-open', pdf_name))
 
-def get_proof():
-    pl.consult("../tests.pl")
-    pl.consult("build_proof")
-    query = "q4(X), buildProof(X, S)"
-    q = list(pl.query(query))
-    return q[0]["S"].decode('UTF-8')
+def get_proofs():
+    text = ""
+    for i in range(1, 12):
+        text += "\\paragraph{Proof " + str(i) + "}"
+        query = "q" + str(i) + "(X), buildProof(X, S)"
+        q = list(pl.query(query))
+        text += q[0]["S"].decode('UTF-8')
+    text += "\\\\ \\vspace{3mm}"
+    return text
 
 def build_file():
-    import shutil
     shutil.copy('preamble.tex', 'proof.tex')
-    text = '\n\\begin{document}\n' + get_proof() + '\n\\end{document}\n'
+    text = '\n\\begin{document}\n' + get_proofs() + '\n\\end{document}\n'
     with open('proof.tex', 'a') as file:
         file.write(text)
 
