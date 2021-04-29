7945e6f2781fea2c5e13c68d0c91bbb5b334d5b8
Flip Lijnzaad <f.a.e.lijnzaad@student.rug.nl>
2021-04-29 11:54:08 +0200

    Changed character escaping, added non-breaking space

--- interface/parse_justifications.pl
+++ interface/parse_justifications.pl
@@ -1,16 +1,9 @@
-:- set_prolog_flag(character_escapes, off).
-:- set_prolog_flag(answer_write_options,
-                    [ quoted(false),
-                      portray(true),
-                      spacing(next_argument)
-                    ]).
-
-stringCon("conj", "\land").
-stringCon("disj", "\lor").
-stringCon("neg", "\lnot").
-stringCon("imp", "\lif").
-stringCon("biimp", "\liff").
-stringCon("contra", "\lfalse").
+stringCon("conj", "\\land").
+stringCon("disj", "\\lor").
+stringCon("neg", "\\lnot").
+stringCon("imp", "\\lif").
+stringCon("biimp", "\\liff").
+stringCon("contra", "\\lfalse").
 
 stringJust(premise, "") :- !.
 stringJust(reit, "Reit") :- !.
@@ -19,10 +12,10 @@ stringJust(Justification, String) :-
     sub_string(Justification, _, 5, 0, "Intro"), !,
     sub_string(Justification, 0, _, 5, Connective),
     stringCon(Connective, C),
-    atomics_to_string([C, "Intro"], " ", String).
+    atomics_to_string([C, "~", "Intro"], "", String).
 
 stringJust(Justification, String) :-
     sub_string(Justification, _, 4, 0, "Elim"), !,
     sub_string(Justification, 0, _, 4, Connective),
     stringCon(Connective, C),
-    atomics_to_string([C, "Elim"], " ", String).
+    atomics_to_string([C, "~", "Elim"], "", String).
