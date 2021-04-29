de5c944cda84b106ad2a4a77898ad707e5956882
Flip Lijnzaad <f.a.e.lijnzaad@student.rug.nl>
2021-04-29 18:28:35 +0200

    Included the line-number functionality

--- interface/build_proof.pl
+++ interface/build_proof.pl
@@ -7,32 +7,32 @@
                     ]).
 
 % return the code for one line in String
-oneLine(Formula, Justification, String) :-
+oneLine(N, Formula, Justification, String) :-
     stringFormula(Formula, F),
     stringJust(Justification, J),
-    atomics_to_string(["    &", F, "&", J], " ", String).
+    atomics_to_string(["    & ", N, ". ", F, " & ", J], "", String).
 
 % base case for building the last premise
-buildPremises([line(X, J)|T], T, Build, String) :-
-    oneLine(X, J, Line),
+buildPremises([line(N, X, J)|T], T, Build, String) :-
+    oneLine(N, X, J, Line),
     atomics_to_string([Build, Line, "\n"], "", String),
-    T = [line(_, Just)|_],
+    T = [line(_, _, Just)|_],
     Just \= premise, !.
 
 % recursive case for building premises
-buildPremises([line(X, J)|T], Rest, Build, String) :-
-    oneLine(X, J, Line),
+buildPremises([line(N, X, J)|T], Rest, Build, String) :-
+    oneLine(N, X, J, Line),
     atomics_to_string([Build, Line, " \\\\ \n"], "", String1),
     buildPremises(T, Rest, String1, String).
 
 % base case for building normal lines
-buildLines([line(X, J)|[]], Build, String) :-
-    oneLine(X, J, Line),
+buildLines([line(N, X, J)|[]], Build, String) :-
+    oneLine(N, X, J, Line),
     atomics_to_string([Build, Line, "\n"], "", String), !.
 
 % recursive case for building normal lines
-buildLines([line(X, J)|T], Build, String) :-
-    oneLine(X, J, Line),
+buildLines([line(N, X, J)|T], Build, String) :-
+    oneLine(N, X, J, Line),
     atomics_to_string([Build, Line, " \\\\ \n"], "", String1),
     buildLines(T, String1, String).
 
