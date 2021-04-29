07cfc888cc7e4103357616027c667c17c1b42ea1
Flip Lijnzaad <f.a.e.lijnzaad@student.rug.nl>
2021-04-29 10:35:06 +0200

    Added cuts, fixed escaping backslashes

--- interface/parse_formulas.pl
+++ interface/parse_formulas.pl
@@ -1,26 +1,33 @@
+:- set_prolog_flag(character_escapes, off).
+:- set_prolog_flag(answer_write_options,
+                    [ quoted(false),
+                      portray(true),
+                      spacing(next_argument)
+                    ]).
+
 % the cut is needed to not consider the other option with capitalization
-stringOf(contra, "\\lfalse") :- !.
+stringOf(contra, "\lfalse") :- !.
 
 % parse an atomic sentence to a capitalized string
 stringOf(X, String) :-
     % only do this for atomic sentences
-    atom(X),
+    atom(X), !,
     % make the string uppercase
     string_upper(X, String).
 
 % parse negation
 stringOf(neg(X), String) :-
-    stringOf(X, StringX),
+    !, stringOf(X, StringX),
     % no parentheses needed around negated sentences because binary connectives
     % put parentheses around the subparts already
-    atomics_to_string(["\\lnot", StringX], " ", String).
+    atomics_to_string(["\lnot", StringX], " ", String).
 
 % parse binary connectives
 stringOf(Formula, String) :-
     % get the name of the binary connective
     functor(Formula, Name, 2),
     % put \l in front of it (\land, \lor, etc.)
-    string_concat("\\l", Name, Connective),
+    string_concat("\l", Name, Connective),
     % get the arguments of the connective
     arg(1, Formula, X),
     arg(2, Formula, Y),
