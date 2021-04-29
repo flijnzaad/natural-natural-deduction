3c4cb45fcb4c96108adffebfa1c525e356ef8eab
Flip Lijnzaad <f.a.e.lijnzaad@student.rug.nl>
2021-04-29 11:56:32 +0200

    Removed old things, added working test for proof building

--- interface/interface.py
+++ interface/interface.py
@@ -20,23 +20,12 @@ def compile_open_pdf():
     else:                                       # linux variants
         subprocess.call(('xdg-open', filepath))
 
-def get_formula():
-    pl.consult("parse_formulas.pl")
-    query = "stringOf(if(neg(p), and(p, or(p, iff(r, contra)))), X)"
+def get_proof():
+    pl.consult("../tests.pl")
+    pl.consult("build_proof")
+    query = "q4(X), buildProof(X, S)"
     q = list(pl.query(query))
-    # convert byte string to regular string
-    f = q[0]["X"].decode('UTF-8')
-    print(f)
+    p = q[0]["S"].decode('UTF-8')
+    print(p)
 
-# test query
-query = "provesWrap([line(p, premise), line(q, premise), line(r, premise)], line(and(and(p, q), r), _), X)."
-q = list(pl.query(query))
-proof = []
-
-for line in q[0]["X"]:
-    # this may need updating once subproofs are implemented
-    proof.append(line.value)
-
-print(proof)
-
-get_formula()
+get_proof()
