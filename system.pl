2b485a3d14d4ed1d4b9a8d2841a5934ea52cf342
Flip Lijnzaad <f.a.e.lijnzaad@student.rug.nl>
2021-04-30 17:40:50 +0200

    3.5: Fixed line numbers

--- system.pl
+++ system.pl
@@ -1,4 +1,4 @@
-% version 3.4
+% version 3.5
 
 % This makes sure that answers are never abbreviated with "..."
 :- set_prolog_flag(answer_write_options,
@@ -27,74 +27,74 @@ proves(ProofLines, Available, Line, [Line|ProofLines], _) :-
     Line = line(Next, contra, contraIntro, two(N1, N2)),
     member(line(N1, X, _, _), Available),
     member(line(N2, neg(X), _, _), Available),
-    nextLineNumber(ProofLines, Next).
+    nextLineNumber(Available, Next).
 
 % contradiction elimination:
 proves(ProofLines, Available, Line, [Line|ProofLines], _) :-
     Line = line(Next, _, contraElim, N),
     member(line(N, contra, _, _), Available),
-    nextLineNumber(ProofLines, Next).
+    nextLineNumber(Available, Next).
 
 % conjunction elimination:
 proves(ProofLines, Available, Line, [Line|ProofLines], _) :-
     Line = line(Next, X, conjElim, N),
     member(line(N, and(X,_), _, _), Available),
-    nextLineNumber(ProofLines, Next).
+    nextLineNumber(Available, Next).
 
 proves(ProofLines, Available, Line, [Line|ProofLines], _) :-
     Line = line(Next, Y, conjElim, N),
     member(line(N, and(_,Y), _, _), Available),
-    nextLineNumber(ProofLines, Next).
+    nextLineNumber(Available, Next).
 
 % conjunction introduction:
 proves(ProofLines, Available, Line, [Line|ProofLines], _) :-
     Line = line(Next, and(X, Y), conjIntro, two(N1, N2)),
     member(line(N1, X, _, _), Available),
     member(line(N2, Y, _, _), Available),
-    nextLineNumber(ProofLines, Next).
+    nextLineNumber(Available, Next).
 
 % disjunction introduction:
 proves(ProofLines, Available, Line, [Line|ProofLines], _) :-
     Line = line(Next, or(X, _), disjIntro, N),
     member(line(N, X, _, _), Available),
-    nextLineNumber(ProofLines, Next).
+    nextLineNumber(Available, Next).
 
 proves(ProofLines, Available, Line, [Line|ProofLines], _) :-
     Line = line(Next, or(_, Y), disjIntro, N),
     member(line(N, Y, _, _), Available),
-    nextLineNumber(ProofLines, Next).
+    nextLineNumber(Available, Next).
 
 % implication elimination:
 proves(ProofLines, Available, Line, [Line|ProofLines], _) :-
     Line = line(Next, Y, impElim, two(N1, N2)),
     member(line(N1, if(X, Y), _, _), Available),
     member(line(N2, X, _, _), Available),
-    nextLineNumber(ProofLines, Next).
+    nextLineNumber(Available, Next).
 
 % bi-implication elimination:
 proves(ProofLines, Available, Line, [Line|ProofLines], _) :-
     Line = line(Next, Y, biimpElim, two(N1, N2)),
     member(line(N1, iff(X, Y), _, _), Available),
     member(line(N2, X, _, _), Available),
-    nextLineNumber(ProofLines, Next).
+    nextLineNumber(Available, Next).
 
 proves(ProofLines, Available, Line, [Line|ProofLines], _) :-
     Line = line(Next, X, biimpElim, two(N1, N2)),
     member(line(N1, iff(X, Y), _, _), Available),
     member(line(N2, Y, _, _), Available),
-    nextLineNumber(ProofLines, Next).
+    nextLineNumber(Available, Next).
 
 % negation elimination:
 proves(ProofLines, Available, Line, [Line|ProofLines], _) :-
     Line = line(Next, X, negElim, N),
     member(line(N, neg(neg(X)), _, _), Available),
-    nextLineNumber(ProofLines, Next).
+    nextLineNumber(Available, Next).
 
 % reiteration:
 proves(ProofLines, Available, Line, [Line|ProofLines], _) :-
     Line = line(Next, X, reit, N),
     member(line(N, X, _, _), Available),
-    nextLineNumber(ProofLines, Next).
+    nextLineNumber(Available, Next).
 
 % implication introduction
 proves(ProofLines, Available, Line, End, _) :-
@@ -107,7 +107,7 @@ proves(ProofLines, Available, Line, End, _) :-
     % add the Subproof and Line to the previous lines to get End
     End  = [Line|[Subproof|ProofLines]],
     % calculate the line numbers
-    nextLineNumber(ProofLines, N1),
+    nextLineNumber(Available, N1),
     nextLineNumber([Subproof|ProofLines], Next),
     N2 is Next - 1.
 
