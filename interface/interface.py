9e54dc1b3e90b4037dcd6263ac49971ae8a9f385
Flip Lijnzaad <f.a.e.lijnzaad@student.rug.nl>
2021-05-01 12:18:12 +0200

    Added KeyboardInterrupt catch, improved process printing

--- interface/interface.py
+++ interface/interface.py
@@ -1,10 +1,11 @@
 from pyswip import Prolog       # querying our knowledge bases
 pl = Prolog()
 import os, subprocess, platform # compiling and opening a LaTeX pdf
+import sys                      # handling keyboardinterrupts
 import shutil                   # copying the 'preamble.tex' file
 
 pl.consult("../system.pl")      # load the relevant knowledge bases
-pl.consult("../tests.pl")
+pl.consult("../queries.pl")
 pl.consult("build_proof.pl")
 
 # compiling and opening a LaTeX pdf
@@ -24,9 +25,11 @@ def compile_open_pdf(name):
 # returns a strings that contains the proof(s)
 def get_proofs():
     text = ""
-    for i in range(1, 16+1):
+    for i in range(16, 20+1):
+        query_name = "q" + str(i)
+        print(query_name + ":")
         text += "\\paragraph{Proof " + str(i) + "}"
-        query = "q" + str(i) + "(X), buildProof(X, S)"
+        query = query_name + "(X), buildProof(X, S)"
         q = list(pl.query(query))
         text += q[0]["S"].decode('UTF-8')
     return text
@@ -38,8 +41,19 @@ def build_file():
     with open('proof.tex', 'a') as file:
         file.write(text)
 
-build_file()
-compile_open_pdf('proof')
+def main():
+    build_file()
+    compile_open_pdf('proof')
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
 
 # TODO: add commands that remove the aux and log files (comment it out because
 # of debugging purposes, you want to be able to view the .log file) 
