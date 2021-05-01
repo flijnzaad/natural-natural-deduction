a0c6775392c71a7747fd22cebacd43145a7b9e46
Flip Lijnzaad <f.a.e.lijnzaad@student.rug.nl>
2021-05-01 12:46:16 +0200

    Added functionality to make the tex of only one proof and not compile/open

--- interface/interface.py
+++ interface/interface.py
@@ -22,28 +22,42 @@ def compile_open_pdf(name):
     else:                                       # linux variants
         subprocess.call(('xdg-open', pdf_name))
 
-# returns a strings that contains the proof(s)
-def get_proofs():
-    text = ""
-    for i in range(16, 20+1):
+# returns a string that contains proof i
+def get_proof(i):
+    query = "q" + str(i) + "(X), buildProof(X, S)"
+    q = list(pl.query(query))
+    return q[0]["S"].decode('UTF-8')
+
+# returns a string that contains the proofs in the given range
+def get_proofs(begin, end):
+    all = ""
+    for i in range(begin, end+1):
         query_name = "q" + str(i)
         print(query_name + ":")
         text += "\\paragraph{Proof " + str(i) + "}"
-        query = query_name + "(X), buildProof(X, S)"
-        q = list(pl.query(query))
-        text += q[0]["S"].decode('UTF-8')
-    return text
+        all += get_proof(i)
+    return all
 
-# add the preamble to the proof(s) and surround with begin/end document
-def build_file():
-    shutil.copy('preamble.tex', 'proof.tex')
-    text = '\n\\begin{document}\n' + get_proofs() + '\n\\end{document}\n'
-    with open('proof.tex', 'a') as file:
+# build a document with only the code of one proof
+def build_doc_proof(i):
+    filename = "q" + str(i) + '.tex'
+    with open(filename, 'w') as file:
+        file.write(get_proof(i))
+    print("Succesfully made a proof for q" + str(i) + ", to be found in " +
+            filename)
+
+# build a document with all proofs in the range, add the preamble and surround
+# with begin/end document
+def build_doc_complete(begin, end):
+    shutil.copy('preamble.tex', 'proofs.tex')
+    text = '\n\\begin{document}\n' + get_proofs(begin, end) + '\n\\end{document}\n'
+    with open('proofs.tex', 'a') as file:
         file.write(text)
 
 def main():
-    build_file()
-    compile_open_pdf('proof')
+    # build_doc_complete(16, 20)
+    # compile_open_pdf('proofs')
+    build_doc_proof(19)
 
 if __name__ == '__main__':
     try:
