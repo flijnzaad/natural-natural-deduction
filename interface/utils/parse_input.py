333cbc46c31544d95aad7439f7daf8ca3e071448
Flip Lijnzaad <f.a.e.lijnzaad@student.rug.nl>
2021-05-06 17:42:28 +0200

    Improved, kind of working; made recursive function with regex

--- interface/utils/parse_input.py
+++ interface/utils/parse_input.py
@@ -1,18 +1,36 @@
 import re
 
-# binary connectives
-def parse_binary_sentence(line):
-    regex_binary = "\((.*) (\\land|\\lor|\\lif|\\liff) (.*)\)"
-    x = re.match(regex_binary, line)
-    if x:
-        argument_1 = x[1]
-        connective = x[2]
-        argument_2 = x[3]
-        return argument_1, connective, argument_2
+def connective_to_functor(connective):
+    if connective != "\\lnot":
+        return connective[2:]
+    else:
+        return "neg"
+
+def parse_sentence(line):
+    regex_atomic = r"\s*([A-Z]|\\lfalse)\s*"
+    regex_unary  = r"\s*(\\lnot)\s*(\(.*\))\s*"
+    regex_binary = r"\s*\((.*)\s*(\\land|\\lor|\\lif|\\liff)\s*(.*)\)\s*"
+    atomic       = re.match(regex_atomic, line)
+    unary        = re.match(regex_unary,  line)
+    binary       = re.match(regex_binary, line)
+    if atomic:
+        # TODO: consider contra
+        # TODO: make lowercase
+        return atomic[1]
+    if unary:
+        connective = unary[1]
+        argument   = unary[2]
+        return connective_to_functor(connective) + "(" + parse_sentence(argument) + ")"
+    if binary:
+        argument_1 = binary[1]
+        connective = binary[2]
+        argument_2 = binary[3]
+        return connective_to_functor(connective) + "(" + parse_sentence(argument_1) + "," + parse_sentence(argument_2) + ")"
     else:
         return False
 
 def make_premise(line):
+    print("make premise")
     # add 'line(...)' and line numbers and premise
 
 def read_premises():
@@ -29,9 +47,9 @@ def read_conclusion():
     return conclusion
 
 def input_interface():
-    p = read_premises()
+    # p = read_premises()
     c = read_conclusion()
-    print(p)
-    print(c)
+    # print(p)
+    print(parse_sentence(c))
 
 input_interface()
