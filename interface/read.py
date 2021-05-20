157887e773eb4403222dad565ffa38af4c918bb8
Flip Lijnzaad <f.a.e.lijnzaad@student.rug.nl>
2021-05-20 16:03:15 +0200

    Changed to fit purpose: added all tokens needed

--- interface/read.py
+++ interface/read.py
@@ -1,28 +1,32 @@
 tokens = (
-    'NAME','NUMBER',
-    'PLUS','MINUS','TIMES','DIVIDE','EQUALS',
-    'LPAREN','RPAREN',
+    'PROP',
+    'NOT',
+    'AND',
+    'OR',
+    'IF',
+    'IFF',
+    'FALSE',
+    'LPAREN',
+    'RPAREN'
     )
 
 # Tokens
 
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_TIMES   = r'\*'
-t_DIVIDE  = r'/'
-t_EQUALS  = r'='
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
-t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
-
-def t_NUMBER(t):
-    r'\d+'
-    t.value = int(t.value)
-    return t
+# TODO: add other options for the operators
+t_PROP   = r'[A-Z]'
+t_NOT    = r'\\lnot'
+t_AND    = r'\\land'
+t_OR     = r'\\lor'
+t_IF     = r'\\lif'
+t_IFF    = r'\\liff'
+t_FALSE  = r'\\lfalse'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
 
 # Ignored characters
 t_ignore = " \t"
 
+# for tracking linenumbers
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += t.value.count("\n")
@@ -35,64 +39,13 @@ def t_error(t):
 import ply.lex as lex
 lex.lex()
 
-# Precedence rules for the arithmetic operators
-precedence = (
-    ('left','PLUS','MINUS'),
-    ('left','TIMES','DIVIDE'),
-    ('right','UMINUS'),
-    )
+data = '''((P \land Q )   \lif ( R \lfalse  S))
+'''
 
-# dictionary of names (for storing variables)
-names = { }
-
-def p_statement_assign(p):
-    'statement : NAME EQUALS expression'
-    names[p[1]] = p[3]
-
-def p_statement_expr(p):
-    'statement : expression'
-    print(p[1])
-
-def p_expression_binop(p):
-    '''expression : expression PLUS expression
-                  | expression MINUS expression
-                  | expression TIMES expression
-                  | expression DIVIDE expression'''
-    if p[2] == '+'  : p[0] = p[1] + p[3]
-    elif p[2] == '-': p[0] = p[1] - p[3]
-    elif p[2] == '*': p[0] = p[1] * p[3]
-    elif p[2] == '/': p[0] = p[1] / p[3]
-
-def p_expression_uminus(p):
-    'expression : MINUS expression %prec UMINUS'
-    p[0] = -p[2]
-
-def p_expression_group(p):
-    'expression : LPAREN expression RPAREN'
-    p[0] = p[2]
-
-def p_expression_number(p):
-    'expression : NUMBER'
-    p[0] = p[1]
-
-def p_expression_name(p):
-    'expression : NAME'
-    try:
-        p[0] = names[p[1]]
-    except LookupError:
-        print(f"Undefined name {p[1]!r}")
-        p[0] = 0
-
-def p_error(p):
-    print(f"Syntax error at {p.value!r}")
-
-import ply.yacc as yacc
-yacc.yacc()
+lex.input(data)
 
 while True:
-    try:
-        s = input('calc > ')
-    except EOFError:
+    tok = lex.token()
+    if not tok:
         break
-    yacc.parse(s)
-
+    print(tok)
