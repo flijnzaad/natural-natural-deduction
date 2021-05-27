ab2a5a9785c9ae2b1cc000e4ae215b3ca9e7ce38
Flip Lijnzaad <f.a.e.lijnzaad@student.rug.nl>
2021-05-27 16:32:02 +0200

    Optimization of formatting with format()

--- interface/utils/output.py
+++ interface/utils/output.py
@@ -9,31 +9,33 @@ import re                       # regular expressions
 # remove the .aux and .log files produced by the LaTeX compilation
 def clean_auxiliary_files(name):
     # TODO: maybe this is also OS-specific
-    if os.path.exists(name + ".aux"):
-        os.remove(name + ".aux")
-    if os.path.exists(name + ".log"):
-        os.remove(name + ".log")
+    aux = name + ".aux"
+    log = name + ".log"
+    if os.path.exists(aux): os.remove(aux)
+    if os.path.exists(log): os.remove(log)
 
 # compile and open a LaTeX pdf
 def compile_open_pdf(name):
     # TODO: maybe this is also OS-specific
-    compile_command = 'pdflatex ' + name + '.tex' 
+    compile_command = "pdflatex {}.tex".format(name)
     if USER_MODE: 
         # TODO: and this too
         compile_command += ' -interaction=batchmode 2>&1 > /dev/null'
     if USER_MODE: print("Compiling the pdf...")
     os.system(compile_command)
+
     if USER_MODE: 
         print("Removing auxiliary files...")
         clean_auxiliary_files(name)
-    pdf_name = name + '.pdf'
+
+    pdf_name = name + ".pdf"
     if USER_MODE: print("Opening the pdf...")
-    if platform.system() == 'Darwin':           # macOS
-        subprocess.call(('open', pdf_name))
-    elif platform.system() == 'Windows':        # Windows
+    if platform.system() == "Darwin":           # macOS
+        subprocess.call(("open", pdf_name))
+    elif platform.system() == "Windows":        # Windows
         os.startfile(pdf_name)
     else:                                       # linux variants
-        subprocess.call(('xdg-open', pdf_name))
+        subprocess.call(("xdg-open", pdf_name))
 
 # print usage information
 def print_usage():
@@ -54,7 +56,7 @@ def remove_all():
     # TODO: maybe this is also OS-specific
     files = os.listdir()
     for file in files:
-        if file.startswith("q"):
+        if file.startswith('q'):
             os.remove(file)
             print("Removed", file)
     sys.exit(0)
@@ -71,16 +73,15 @@ def get_last_query_no():
 # return filename with the query number(s)
 def get_filename(numbers):
     if type(numbers) == int:
-        num = str(numbers) 
+        num = str(numbers)
     else:
-        num = str(numbers[0]) + '-' + str(numbers[1])
-    return 'q' + str(num)
+        num = "{}-{}".format(numbers[0], numbers[1])
+    return "q{}".format(num)
 
 # return the filename plus tex extension
 def get_tex_name(numbers):
-    return get_filename(numbers) + '.tex'
+    return get_filename(numbers) + ".tex"
 
 # print a process message that states that a file has been built succesfully
 def print_built_msg(numbers):
-    print("Succesfully built a proof tex file for " + get_filename(numbers) 
-      + ", to be found in " + get_tex_name(numbers))
+    print("Succesfully built a proof tex file for {}, to be found in {}".format(get_filename(numbers), get_tex_name(numbers)))
